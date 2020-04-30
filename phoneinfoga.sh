9a5961cbc45f17f05e5e7e956df83f0f467a1961
abhackerofficial <63346676+abhackerofficial@users.noreply.github.com>
2020-05-01 00:32:08 +0530

    Update phoneinfoga.sh

--- phoneinfoga.sh
+++ phoneinfoga.sh
@@ -14,21 +14,21 @@ echo -e "\x1b[32m
 "
 rm -rf phoneinfoga.sh
 sleep 5.0
-echo -e "\e[32m[*] This Script is written by ABHacker Official\e[39m"
+echo -e '\033[1;32m[''\033[0m*''\033[1;32m]''\033[1;32m This Script is written by ABHacker Official'
 echo
 sleep 2.0
-echo -e "\e[32m[*] PhoneInfoga Installing...\e[39m"
+echo -e '\033[1;32m[''\033[0m*''\033[1;32m]''\033[1;32m PhoneInfoga Installing...'
 sleep 2.0
 echo
-echo -e "\e[32m[*] Please wait...\e[39m"
+echo -e '\033[1;32m[''\033[0m*''\033[1;32m]''\033[1;32m Please wait...'
 sleep 2.0
 echo
 cd
-echo -e "\e[32m[+] Required Packages Installing...\e[39m"
+echo -e '\033[1;32m[''\033[0m*''\033[1;32m]''\033[1;32m Required Packages Installing...'
 apt update > /dev/null 2>&1 && apt --assume-yes upgrade > /dev/null 2>&1 && apt --assume-yes install python2 > /dev/null 2>&1 && apt --assume-yes install python > /dev/null 2>&1 && apt --assume-yes install BeautifulSoup > /dev/null 2>&1 && apt --assume-yes install git > /dev/null 2>&1
 sleep 2.0
 echo
-echo -e "\e[32m[+] Required Packages Installed.\e[39m"
+echo -e '\033[1;32m[''\033[0m*''\033[1;32m]''\033[1;32m Required Packages Installed.'
 rm -rf PhoneInfoga
 rm -rf phoneinfoga
 git clone https://github.com/abhackerofficial/PhoneInfoga > /dev/null 2>&1 && cd PhoneInfoga > /dev/null 2>&1 && unzip PhoneInfoga > /dev/null 2>&1 && python3 -m pip install -r requirements.txt > /dev/null 2>&1 && pip2 install colorama > /dev/null 2>&1
@@ -37,7 +37,7 @@ rm -rf phoneinfoga.sh
 chmod +x *
 sleep 2.0
 echo
-echo -e "\e[32m[*] PhoneInfoga Successfully Installed.\e[39m"
+echo -e '\033[1;32m[''\033[0m*''\033[1;32m]''\033[1;32m PhoneInfoga Successfully Installed.'
 sleep 2.0
 echo
 printf $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Press Enter To Exit: \e[0m'
