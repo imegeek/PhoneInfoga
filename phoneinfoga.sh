1ab7da6237f88034778ee5665f5d2cb99323d5db
abhackerofficial <63346676+abhackerofficial@users.noreply.github.com>
2020-04-30 13:44:26 +0530

    Update phoneinfoga.sh

--- phoneinfoga.sh
+++ phoneinfoga.sh
@@ -16,11 +16,11 @@ echo -e "\e[32m[*] Please wait...\e[39m"
 sleep 2.0
 echo
 cd
-echo -e "\033[1;92m"[+] Required Packages Installing...
+echo -e "\e[32m[+] Required Packages Installing...\e[39m"
 apt update > /dev/null 2>&1 && apt --assume-yes upgrade > /dev/null 2>&1 && apt --assume-yes install python2 > /dev/null 2>&1 && apt --assume-yes install python > /dev/null 2>&1 && apt --assume-yes install BeautifulSoup > /dev/null 2>&1 && apt --assume-yes install git > /dev/null 2>&1
 sleep 2.0
 echo
-echo -e "\033[1;92m"[+] Required Packages Installed. 
+echo -e "\e[32m[+] Required Packages Installed.echo\e[39m"
 rm -rf PhoneInfoga
 rm -rf phoneinfoga
 git clone https://github.com/abhackerofficial/PhoneInfoga > /dev/null 2>&1 && cd PhoneInfoga > /dev/null 2>&1 && unzip PhoneInfoga > /dev/null 2>&1 && python3 -m pip install -r requirements.txt > /dev/null 2>&1
@@ -29,7 +29,7 @@ rm -rf phoneinfoga.sh
 chmod +x *
 sleep 2.0
 echo
-echo -e "\e[32mPhoneInfoga Successfully Installed.\e[39m"
+echo -e "\e[32m[*] PhoneInfoga Successfully Installed.\e[39m"
 sleep 2.0
 echo
 printf $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Press Enter To Exit: \e[0m'
